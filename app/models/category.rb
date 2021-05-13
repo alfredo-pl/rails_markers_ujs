4a8978b242616c26822fd3a825fbafeaa61d78d7
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-13 13:39:48 -0400

    model category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,18 @@
 class Category < ApplicationRecord
-  belongs_to :category
+  belongs_to :category,optional: true
   belongs_to :type
-  has_many :markers
+  has_many :markers , dependent: :destroy
+
+  def name_type
+    Type.references(:category).where(id: type_id).pluck :name
+  end
+  def name_parent(id)
+    
+    if id
+      Category.find_by(id: id).name
+    else
+      ""
+    end
+
+  end
 end
