cd8860e31f372ed266a1541abefd43c0e2be1218
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-13 00:32:15 -0400

    migrate

--- db/migrate/20210513033756_create_categories.rb
+++ db/migrate/20210513033756_create_categories.rb
@@ -2,8 +2,8 @@ class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
       t.string :name
-      t.bolean :public
-      t.references :category, null: false, foreign_key: true
+      t.boolean :public
+      t.references :category, null: true, foreign_key: true
       t.references :type, null: false, foreign_key: true
 
       t.timestamps
---
+++ db/schema.rb
@@ -0,0 +1,44 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_05_13_033850) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.boolean "public"
+    t.integer "category_id"
+    t.integer "type_id", null: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_categories_on_category_id"
+    t.index ["type_id"], name: "index_categories_on_type_id"
+  end
+
+  create_table "markers", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_markers_on_category_id"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "categories", "categories"
+  add_foreign_key "categories", "types"
+  add_foreign_key "markers", "categories"
+end
