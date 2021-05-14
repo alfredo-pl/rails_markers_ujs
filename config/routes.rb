300e535a267af822d8eec5ed8b911b8ac857eec3
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-13 21:56:59 -0400

    root path dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :categories
   resources :types
   get 'data-categories' , to: 'categories#data_categories'
-  root "maker#index"
+  root "dashboard#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
