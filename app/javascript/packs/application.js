1f3f87b728af7195d412acb935c7d1d7e7b5e310
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-13 21:56:09 -0400

    add chartkick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import $ from "jquery"
-window.$ = $;
-
+window.$ = $
+import "chartkick/chart.js"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
